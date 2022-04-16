d8a8dacd3109daffb7b3707358c973c9d0c1bc83
Paulo Ricardo <62405013+Sebola3461@users.noreply.github.com>
2022-04-16 00:26:43 -0300

    Add stamina and speed strain to std extras

--- helpers/messages/calculateExtras.ts
+++ helpers/messages/calculateExtras.ts
@@ -3,7 +3,9 @@ import { StandardDifficultyAttributes } from "osu-standard-stable";
 export default (att: StandardDifficultyAttributes) => {
 	return `OD: ${att.overallDifficulty.toFixed(
 		1
-	)} AR: ${att.approachRate.toFixed(1)}  |  `;
+	)} AR: ${att.approachRate.toFixed(1)} Aim: ${att.aimStrain.toFixed(
+		2
+	)} Speed Strain: ${att.speedStrain.toFixed(2)}  |  `;
 };
 
 export function calculateCTBExtras(od: number, ar: number) {
