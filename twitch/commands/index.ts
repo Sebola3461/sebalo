c931542ab7d48203a82317835661c37c98abd80f
Paulo Ricardo <62405013+Sebola3461@users.noreply.github.com>
2022-04-21 22:39:27 -0300

    Add leaderboard command

--- twitch/commands/index.ts
+++ twitch/commands/index.ts
@@ -2,12 +2,15 @@ import blacklist from "./blacklist";
 import level from "./level";
 import requests from "./requests";
 import stars from "./stars";
+import leaderboard from "./leaderboard";
 
 const commands: any = {
 	requests: requests,
 	stars: stars,
 	level: level,
 	blacklist: blacklist,
+	leaderboard: leaderboard,
+	lb: leaderboard,
 };
 
 export default commands;
---
+++ twitch/commands/leaderboard.ts
@@ -0,0 +1,17 @@
+import { BanchoClient } from "bancho.js";
+import { ChatUserstate, Client } from "tmi.js";
+
+export default async (
+	message: string,
+	tags: ChatUserstate,
+	channel: string,
+	bancho: BanchoClient,
+	client: Client,
+	args: string[]
+) => {
+	return client
+		.say(channel, `Check level leaderboards here: ${channel.slice(1)}`)
+		.catch((e) => {
+			console.log(e);
+		});
+};
