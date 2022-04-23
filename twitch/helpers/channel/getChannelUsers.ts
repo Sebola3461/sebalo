8bb5e0f8dcde3d2075cd452cb6b55f5428e26893
Paulo Ricardo <62405013+Sebola3461@users.noreply.github.com>
2022-04-22 22:31:17 -0300

    prevent crash

--- twitch/helpers/channel/getChannelUsers.ts
+++ twitch/helpers/channel/getChannelUsers.ts
@@ -1,9 +1,25 @@
 import axios from "axios";
 
 export default async (channel: string) => {
-	const u = await axios(
-		`https://tmi.twitch.tv/group/user/${channel}/chatters`
-	);
+	try {
+		const u = await axios(
+			`https://tmi.twitch.tv/group/user/${channel}/chatters`
+		);
 
-	return u.data;
+		return u.data;
+	} catch (e) {
+		return {
+			_links: {},
+			chatter_count: 1,
+			chatters: {
+				broadcaster: [channel],
+				vips: [],
+				moderators: [],
+				staff: [],
+				admins: [],
+				global_mods: [],
+				viewers: [],
+			},
+		};
+	}
 };
