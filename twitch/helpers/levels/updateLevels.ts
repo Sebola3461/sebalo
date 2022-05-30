868c13347438a4b4159b60ce09ad93bcbdd3456b
Paulo Ricardo <62405013+Sebola3461@users.noreply.github.com>
2022-05-29 23:30:05 -0300

    Add me as adm

--- twitch/helpers/levels/updateLevels.ts
+++ twitch/helpers/levels/updateLevels.ts
@@ -43,7 +43,7 @@ export async function updateLevels(
 
 	if (!streamer) return;
 
-	if (streamer.levels.enable == false)
+	if (!streamer.levels.enable)
 		return console.log(
 			`Skipping ${tags.username} in ${channel} cuz levels are disabled...`
 		);
